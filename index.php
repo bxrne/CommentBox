8bbd5732542c01324931c09d70544538e4120c42
Adam Byrne <AdamByrne@users.noreply.github.com>
2014-09-15 18:57:14 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!Doctype HTML>
+<?php
+if ($_POST) {
+	$name = $_POST['commentName'];
+	$txt = $_POST['commentTXT'];
+	$handle = fopen("comments.html","a");
+	fwrite($handle, "<b>" . $name . "</b> :<br/>" . $txt . "<br/>");
+	fclose($handle);
+}
+?>
+<html>
+	<head>
+		<title>PHP Comment box</title>
+	</head>
+	<body>
+		<form action="" method="POST">
+			Name:<br>
+			<input type="text" name="commentName"/><br>
+			Comments:<br>
+			<textarea rows="10" cols="30" name="commentTXT"></textarea><br>
+			<input type="submit" value=":: Post Comment ::">
+		</form><br><br>
+		<hr>
+				<?php include "comments.html";?>
+</html>
